b8c4025eb4b81743abe9cad0f7ff34cd3f9a87c0
didolaneca@yahoo.com <didolaneca@yahoo.com>
2020-02-17 20:56:05 -0600

    Initial commit

---
+++ src/main/java/com/diyan/ctci/ProfileManager.java
@@ -0,0 +1,17 @@
+package com.diyan.ctci;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProfileManager {
+    @Autowired
+    private Environment env;
+
+    @GetMapping("envdetails")
+    public String envDetails(){
+        return env.toString();
+    }
+}
