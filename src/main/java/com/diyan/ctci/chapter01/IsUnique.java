dc523bedd3dcdc82ab911d3209eaae2cf040d4ac
didolaneca@yahoo.com <didolaneca@yahoo.com>
2020-05-18 13:59:43 -0500

    Added a different implementation of the IsUnique problem as well a documentation.

--- src/main/java/com/diyan/ctci/chapter01/IsUnique.java
+++ src/main/java/com/diyan/ctci/chapter01/IsUnique.java
@@ -1,15 +1,36 @@
 package com.diyan.ctci.chapter01;
 
 public class IsUnique {
-    //    public static void main(String[] args) {}
-    public boolean isUniqueChars(String str) {
+    /**
+     *  Problem: Implement an algorithm to determine if a string has all unique characters.
+     *  Ask if the string is ASCII of Unicode. Java uses Unicode.
+     * @param string the string we want to determine if contains all unique characters
+     * @return true if the string contains all unique characters and false otherwise.
+     */
+    public boolean isUniqueChars(String string) {
         boolean[] checks = new boolean[250];
-        for (int i = 0; i < str.length(); i++) {
-            int index = str.charAt(i);
+        for (int i = 0; i < string.length(); i++) {
+            int index = string.charAt(i);
             if (checks[index]) {
                 return false;
             }
             checks[index] = true;
+        }
+        return true;
+    }
+
+    /**
+     * Different implementation of {@link #isUniqueChars(String)}
+     * @param string the string we want to determine if contains all unique characters
+     * @return true if the string contains all unique characters and false otherwise.
+     */
+    public boolean isUniqueCharsBit(String string){
+        int checker = 0;
+        for(int i = 0; i < string.length(); i++){
+            int val = string.charAt(i) - 'a';
+            if((checker & (1 << val)) > 0)
+                return false;
+            checker |= (1 << val);
         }
         return true;
     }
