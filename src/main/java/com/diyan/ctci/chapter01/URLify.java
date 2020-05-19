28ec7c1b0920de95792e90ffb66001835652e1d3
didolaneca@yahoo.com <didolaneca@yahoo.com>
2020-05-19 12:51:31 -0500

    Added alternative implementation and tests for URLify problem

--- src/main/java/com/diyan/ctci/chapter01/URLify.java
+++ src/main/java/com/diyan/ctci/chapter01/URLify.java
@@ -19,7 +19,37 @@ public class URLify {
                 result.append(temp);
             }
         }
-
         return result.toString();
+    }
+
+    /**
+     * Different implementation of the {@link #urlify} problem. This implementation takes
+     * @param string char array and
+     * @param trueLength the string legth plus the white spaces
+     * and modifies the char array
+     */
+    void replaceSpaces(char[] string, int trueLength){
+        int spaceCount = 0,  index, i;
+        //first we count the white spaces in the char array
+        for (i = 0; i < trueLength; i++){
+            if(string[i] == 32){
+                spaceCount++;
+            }
+        }
+        index = trueLength + spaceCount * 2;
+        if(trueLength < string.length){
+            string[trueLength] = '\0';
+        }
+        for(i = trueLength - 1;  i >= 0; i--){
+            if(string[i] == 32){
+                string[index - 1] = '0';
+                string[index - 2] = '2';
+                string[index - 3] = '%';
+                index -= 3;
+            } else {
+                string[index - 1] = string[i];
+                index--;
+            }
+        }
     }
 }
--- src/test/java/com/diyan/ctci/chapter01/URLifyTests.java
+++ src/test/java/com/diyan/ctci/chapter01/URLifyTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
+
 public class URLifyTests {
     URLify urLify;
 
@@ -22,5 +24,17 @@ public class URLifyTests {
         Assertions.assertEquals("I%20like%20going%20to%20the%20mall%20for%20ice%20cream", result2);
     }
 
+    @Test
+    void replaceSpacesPositiveTest() {
+        char[] test1 = new char[]{'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', ' ', ' '};
+        urLify.replaceSpaces(test1, 11);
+        Assertions.assertArrayEquals(new char[]{'H', 'e', 'l', 'l', 'o', '%', '2', '0', 'W', 'o', 'r', 'l', 'd'}, test1);
+        char[] test2 = new char[]{'D', 'i', 'd', 'o', ' ', 'i', 's', ' ', 'a', 'w', 'e', 's', 'o', 'm', 'e', ' ', ' ', ' ', ' '};
+        urLify.replaceSpaces(test2, 15);
+        Assertions.assertArrayEquals(new char[]{'D', 'i', 'd', 'o', '%', '2', '0', 'i', 's', '%', '2', '0', 'a', 'w', 'e', 's', 'o', 'm', 'e'}, test2);
 
+        char[] test3 = new char[]{'I', ' ', 'l', 'i', 'k', 'e', ' ', 'g', 'o', 'i', 'n', 'g', ' ', 't', 'o', ' ', 't', 'h', 'e', ' ', 'm', 'a', 'l', 'l', ' ', 'f', 'o', 'r', ' ', 'i', 'c', 'e', ' ', 'c', 'r', 'e', 'a', 'm', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+        urLify.replaceSpaces(test3, 38);
+        Assertions.assertArrayEquals(new char[]{'I', '%', '2', '0', 'l', 'i', 'k', 'e', '%', '2', '0', 'g', 'o', 'i', 'n', 'g', '%', '2', '0', 't', 'o', '%', '2', '0', 't', 'h', 'e', '%', '2', '0', 'm', 'a', 'l', 'l', '%', '2', '0', 'f', 'o', 'r', '%', '2', '0', 'i', 'c', 'e', '%', '2', '0', 'c', 'r', 'e', 'a', 'm'}, test3);
+    }
 }
