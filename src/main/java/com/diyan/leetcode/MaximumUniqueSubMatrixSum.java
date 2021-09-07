10438b936fb6f52903c797aa32ef0c4bce35ca50
didolaneca@yahoo.com <didolaneca@yahoo.com>
2021-09-07 01:10:28 -0500

    Current work on the matrix problem

---
+++ src/main/java/com/diyan/leetcode/MaximumUniqueSubMatrixSum.java
@@ -0,0 +1,27 @@
+package com.diyan.leetcode;
+
+public class MaximumUniqueSubMatrixSum {
+    public static void main(String[] args) {
+        int[][] matrixOne = {
+                {1,3,5,6},
+                {5,1,3,8},
+                {1,8,3,6},
+                {7,4,3,6}
+        };
+        System.out.println(SumOfUniqueElementsInASubMatrixWithMaxSum(matrixOne, 2));
+    }
+
+    /**
+     * Given a two-dimensional array N x N and a number < N.
+     * Find the sum of all unique elements of all the sub-matrices with maximum sum of all their elements
+     * @param matrix
+     * @param num
+     * @return
+     */
+    static int SumOfUniqueElementsInASubMatrixWithMaxSum(int[][] matrix, int num){
+
+        return 0;
+    }
+
+
+}
